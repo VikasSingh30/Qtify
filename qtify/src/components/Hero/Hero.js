8f62d084dd8934f920a4a9f5bb483c655b04cc3f
Vikas <singhvikas.sv30@gmail.com>
2024-08-26 15:16:37 +0000

    Updated

--- qtify/src/components/Hero/Hero.js
+++ qtify/src/components/Hero/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import HeroImage from '../../assets/vibrating-headphone.png'
+import HeroImage from '../../assets/vibrating-headphones.png'
 import styles from './Hero.module.css'
 const Hero = () => {
   return (
